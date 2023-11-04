87fd4b901bb2ecaa5ddc1363b232ba9ce8901e92
Gayatri <gayatridwankhade@gmail.com>
2023-11-04 13:32:51 +0530

    footer completed

---
+++ script.js
@@ -0,0 +1,16 @@
+// gsap.from(".text",{
+//     opacity:0
+// })
+
+gsap.from(".text h1",{
+    y:100,
+    delay:1,
+    duration:2,
+    stagger:1
+})
+// gsap.from("#section-one p",{
+//     y:100,
+//     delay:1,
+//     duration:2,
+//     stagger:1
+// })
